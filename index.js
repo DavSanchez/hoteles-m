08bdf5b8a43f22e1fd0ce493862447d17afa49d0
David <d.vinternatt@gmail.com>
2018-01-23 12:13:44 +0000

    adding database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const pg = require('pg')
 const PORT = process.env.PORT || 5000
 
 express()
@@ -7,4 +8,15 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query('SELECT * FROM test_table', function(err, result) {
+        done();
+        if (err)
+         { console.error(err); response.send("Error " + err); }
+        else
+         { response.render('pages/db', {results: result.rows} ); }
+      });
+    });
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
