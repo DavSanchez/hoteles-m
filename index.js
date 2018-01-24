074920fd261303bc191fbace07a3275b26113c4f
David <d.vinternatt@gmail.com>
2018-01-24 13:43:43 +0000

    Formatting

--- index.js
+++ index.js
@@ -13,36 +13,30 @@ express()
   //habsimple para habitacion simple
   //playa para excursion a la playa
   .get('/habdoble', function (request, response) {
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-      client.query('SELECT * FROM servicios_table WHERE id_servicio=1', function(err, result) {
+    pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+      client.query('SELECT * FROM servicios_table WHERE id_servicio=1', function (err, result) {
         done();
-        if (err)
-         { console.error(err); response.send("Error " + err); }
-        else
-         { response.render('pages/habdoble', {results: result.rows} ); }
+        if (err) { console.error(err); response.send("Error " + err); }
+        else { response.render('pages/habdoble', { results: result.rows }); }
       });
     });
   })
   .get('/habsimple', function (request, response) {
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-      client.query('SELECT * FROM servicios_table WHERE id_servicio=2', function(err, result) {
+    pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+      client.query('SELECT * FROM servicios_table WHERE id_servicio=2', function (err, result) {
         done();
-        if (err)
-         { console.error(err); response.send("Error " + err); }
-        else
-         { response.render('pages/habsimple', {results: result.rows} ); } // TODO
+        if (err) { console.error(err); response.send("Error " + err); }
+        else { response.render('pages/habsimple', { results: result.rows }); } // TODO
       });
     });
   })
   .get('/playa', function (request, response) {
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-      client.query('SELECT * FROM servicios_table WHERE id_servicio=3', function(err, result) {
+    pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+      client.query('SELECT * FROM servicios_table WHERE id_servicio=3', function (err, result) {
         done();
-        if (err)
-         { console.error(err); response.send("Error " + err); }
-        else
-         { response.render('pages/playa', {results: result.rows} ); } // TODO
+        if (err) { console.error(err); response.send("Error " + err); }
+        else { response.render('pages/playa', { results: result.rows }); } // TODO
       });
     });
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
