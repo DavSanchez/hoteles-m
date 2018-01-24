01ecc614d303a81892f2bd38ac5e683abed4e03b
David <d.vinternatt@gmail.com>
2018-01-24 19:38:36 +0000

    back to original...

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const path = require('path')
 const pg = require('pg')
-const pgp = require('pg-promise')({})
-pgp.pg.defaults.poolSize = 20
-const db = pgp(process.env.DATABASE_URL);
+//const pgp = require('pg-promise')({})
+//pgp.pg.defaults.poolSize = 20
+//const db = pgp(process.env.DATABASE_URL);
 const PORT = process.env.PORT || 5000
 
 express()
@@ -11,41 +11,34 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/redir', (req, res) => res.render('pages/redir'))
-  .get('/habdoble', function (request, response) {
-    db.any('SELECT * FROM servicios_table WHERE id_servicio=1')
-    .then(function(data) {
-        // success;
-        response.render('pages/habdoble', { results: result.rows });
-    })
-    .catch(function(error) {
-        // error;
-        console.error(err); response.send("Error " + err);
+  .get('/redir', (req, res) => res.render('pages/redir')).get('/habdoble', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+      client.query('SELECT * FROM servicios_table WHERE id_servicio=1', function (err, result) {
+        done();
+        if (err) { console.error(err); response.send("Error " + err); }
+        else { response.render('pages/habdoble', { results: result.rows }); }
+      });
     });
   })
   .get('/habsimple', function (request, response) {
-    db.any('SELECT * FROM servicios_table WHERE id_servicio=2')
-    .then(function(data) {
-        // success;
-        response.render('pages/habsimple', { results: result.rows });
-    })
-    .catch(function(error) {
-        // error;
-        console.error(err); response.send("Error " + err);
+    pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+      client.query('SELECT * FROM servicios_table WHERE id_servicio=2', function (err, result) {
+        done();
+        if (err) { console.error(err); response.send("Error " + err); }
+        else { response.render('pages/habsimple', { results: result.rows }); }
+      });
     });
   })
   .get('/playa', function (request, response) {
-    db.any('SELECT * FROM servicios_table WHERE id_servicio=3')
-    .then(function(data) {
-        // success;
-        response.render('pages/playa', { results: result.rows });
-    })
-    .catch(function(error) {
-        // error;
-        console.error(err); response.send("Error " + err);
+    pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+      client.query('SELECT * FROM servicios_table WHERE id_servicio=3', function (err, result) {
+        done();
+        if (err) { console.error(err); response.send("Error " + err); }
+        else { response.render('pages/playa', { results: result.rows }); }
+      });
     });
   })
-  .get('/admin', function (request, response) {  // TODO
+  /*.get('/admin', function (request, response) {  // TODO
     db.multi('SELECT * FROM administrador_table; SELECT * FROM clientes_table; SELECT * FROM servicios_table;')
       .then(data => {
         // data[0] = result from the first query;
@@ -59,5 +52,5 @@ express()
       .catch(error => {
         console.error(err); response.send("Error " + err); // error
       });
-  })
+  })*/
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
