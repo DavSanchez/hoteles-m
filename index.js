b61007a39864ac86d37818076b14ce062d1f009a
David <d.vinternatt@gmail.com>
2018-01-25 17:21:11 +0000

    Code formatting

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const path = require('path')
 const pg = require('pg')
 const pgp = require('pg-promise')()
-pgp.pg.defaults.poolSize = 20;
-const db = pgp(process.env.DATABASE_URL);
-const PORT = process.env.PORT || 5000 // TEST COMMENT
+pgp.pg.defaults.poolSize = 20
+const db = pgp(process.env.DATABASE_URL)
+const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -15,43 +15,44 @@ express()
   .get('/habdoble', function (request, response) {
     db.any('SELECT * FROM servicios_table WHERE id_servicio=1')
     .then(data => {
-      response.render('pages/habdoble', { results: data });
+      response.render('pages/habdoble', { results: data })
     })
     .catch(err => {
-      console.error(err); response.send("Error " + err); // error
-    });
+      console.error(err); response.send("Error " + err)
+    })
   })
   .get('/habsimple', function (request, response) {
     db.any('SELECT * FROM servicios_table WHERE id_servicio=2')
     .then(data => {
-      response.render('pages/habsimple', { results: data });
+      response.render('pages/habsimple', { results: data })
     })
     .catch(err => {
-      console.error(err); response.send("Error " + err); // error
-    });
+      console.error(err); response.send("Error " + err)
+    })
   })
   .get('/playa', function (request, response) {
     db.any('SELECT * FROM servicios_table WHERE id_servicio=3')
     .then(data => {
-      response.render('pages/playa', { results: data });
+      response.render('pages/playa', { results: data })
     })
     .catch(err => {
-      console.error(err); response.send("Error " + err); // error
-    });
+      console.error(err); response.send("Error " + err)
+    })
   })
-  .get('/admin', function (request, response) {  // TODO
+  .get('/admin', function (request, response) {
     db.multi('SELECT * FROM administrador_table; SELECT * FROM cliente_table; SELECT * FROM servicios_table;')
       .then(data => {
-        // data[0] = result from the first query;
-        // data[1] = result from the second query;
+        // data[0] = result from the first query
+        // data[1] = result from the second query
+        // ...
         response.render('pages/admin', {
           resultsAdmin: data[0],
           resultsClientes: data[1],
           resultsServicios: data[2]
-        });
+        })
       })
       .catch(err => {
-        console.error(err); response.send("Error " + err); // error
-      });
+        console.error(err); response.send("Error " + err)
+      })
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
