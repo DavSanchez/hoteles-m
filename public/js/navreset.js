7c51978256debf3a4d67da65cce46fe6c1da8870
David Sánchez <David@iMac-de-Casa.local>
2018-01-25 02:38:13 +0000

    Changed links and removed useless file

--- public/js/navreset.js
+++ public/js/navreset.js
@@ -7,9 +7,9 @@ const id = val => document.getElementById(val),
     navContacto = id("contacto");
 
 window.onload = function () {
-    navHome.href = "https://rocky-garden-75734.herokuapp.com/";
-    navInicio.href = "https://rocky-garden-75734.herokuapp.com/#jumbotron";
-    navServicios.href = "https://rocky-garden-75734.herokuapp.com/#about";
-    navContacto.href = "https://rocky-garden-75734.herokuapp.com/#footer";
+    navHome.href = "/";
+    navInicio.href = "/#jumbotron";
+    navServicios.href = "/#about";
+    navContacto.href = "/#footer";
 }
 
